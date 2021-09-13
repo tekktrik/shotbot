62cd9cd22765e9fee172fe416ece62e6f94e8e46
Tekktrik <tekktrik@gmail.com>
2021-09-12 23:36:43 -0400

    Working on eeprom class

--- code/coin_op_attachment.py
+++ code/coin_op_attachment.py
@@ -1,12 +1,19 @@
 from coin_acceptor import CoinAcceptor
 from led_backpack import LEDBackpack
+from i2c_eeprom import EEPROMBreakout
 
 class CoinOpAttachment:
 
-    def __init__(self, pulse_gpio, led_i2c):
+    def __init__(self, eeprom_i2c_address, pulse_gpio, led_i2c_address):
         
-        self._i2c = i2c_object
-        self._coin_acceptor = CoinAcceptor(pulse_gpio)
-        self._led_backpack = LEDBackpack(led_i2c)
+        self.eeprom = EEPROMBreakout(eeprom_i2c_address)
+        self.coin_acceptor = CoinAcceptor(pulse_gpio)
+        self.led_backpack = LEDBackpack(led_i2c_address)
         
-    def check for 
+    def initialize(self):
+        
+        stored_data = self.eeprom.readAllData()
+        
+    def checkCoinInserted(self):
+        
+        self.
